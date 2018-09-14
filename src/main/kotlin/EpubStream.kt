26aff5da0165408d37882ee714994fe2d7ab7a0e
JSpiner <jspiner@naver.com>
2018-09-14 18:36:50 +0900

    ePub 기본 stream 함수 추가

--- src/main/kotlin/EpubStream.kt
+++ src/main/kotlin/EpubStream.kt
@@ -1,5 +1,31 @@
+import io.reactivex.Completable
+import io.reactivex.Single
 import java.io.File
 
 class EpubStream(val file : File) {
+
+    fun unzip() : Completable {
+        return Completable.complete()
+    }
+
+    fun getMimeType() : Single<Any> {
+        return Single.just(1)
+    }
+
+    fun getContainer() : Single<Any> {
+        return Single.just(1)
+    }
+
+    fun getOpf() : Single<Any> {
+        return Single.just(1)
+    }
+
+    fun getToc() : Single<Any> {
+        return Single.just(1)
+    }
+
+    fun getFile() : Single<Any> {
+        return Single.just(1)
+    }
 
 }
--- src/test/kotlin/EpubStreamInitTest.kt
+++ src/test/kotlin/EpubStreamInitTest.kt
@@ -1,8 +1,66 @@
 import org.junit.Test
 import java.io.File
 
-@Test
-fun initSuccessTest() {
-    val dummyFile = File("file://dummy/file")
-    EpubStream(dummyFile)
+class EpubStreamInitTest {
+
+    @Test
+    fun initSuccessTest() {
+        val dummyFile = File("file://dummy/file")
+        EpubStream(dummyFile)
+    }
+
+    @Test
+    fun unzipSuccessTest() {
+        val dummyFile = File("file://dummy/file")
+        val epubStream = EpubStream(dummyFile)
+        epubStream.unzip()
+                .test()
+                .assertResult()
+    }
+
+    @Test
+    fun getMimeTypeSuccessTest() {
+        val dummyFile = File("file://dummy/file")
+        val epubStream = EpubStream(dummyFile)
+        epubStream.getMimeType()
+                .test()
+                .assertResult(1)
+    }
+
+    @Test
+    fun getContainerSuccessTest() {
+        val dummyFile = File("file://dummy/file")
+        val epubStream = EpubStream(dummyFile)
+        epubStream.getContainer()
+                .test()
+                .assertResult(1)
+    }
+
+    @Test
+    fun getOpfSuccessTest() {
+        val dummyFile = File("file://dummy/file")
+        val epubStream = EpubStream(dummyFile)
+        epubStream.getOpf()
+                .test()
+                .assertResult(1)
+    }
+
+    @Test
+    fun getTocSuccessTest() {
+        val dummyFile = File("file://dummy/file")
+        val epubStream = EpubStream(dummyFile)
+        epubStream.getToc()
+                .test()
+                .assertResult(1)
+    }
+
+    @Test
+    fun getFileSuccessTest() {
+        val dummyFile = File("file://dummy/file")
+        val epubStream = EpubStream(dummyFile)
+        epubStream.getFile()
+                .test()
+                .assertResult(1)
+    }
+
 }
