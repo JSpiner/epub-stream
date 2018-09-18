909196fdd90c9c9433dae23e84289820fdb6d46b
JSpiner <jspiner@naver.com>
2018-09-18 12:49:28 +0900

    임시로 만들어둔 테스트코드 제거

--- src/test/kotlin/net/jspiner/epubstream/EpubStreamInitTest.kt
+++ src/test/kotlin/net/jspiner/epubstream/EpubStreamInitTest.kt
@@ -1,6 +1,5 @@
 package net.jspiner.epubstream
 
-import net.jspiner.epubstream.EpubStream
 import org.junit.Test
 import java.io.File
 
@@ -11,41 +10,4 @@ class EpubStreamInitTest {
         val dummyFile = File("file://dummy/file")
         EpubStream(dummyFile)
     }
-
-    @Test
-    fun getContainerSuccessTest() {
-        val dummyFile = File("file://dummy/file")
-        val epubStream = EpubStream(dummyFile)
-        epubStream.getContainer()
-                .test()
-                .assertResult(1)
-    }
-
-    @Test
-    fun getOpfSuccessTest() {
-        val dummyFile = File("file://dummy/file")
-        val epubStream = EpubStream(dummyFile)
-        epubStream.getOpf()
-                .test()
-                .assertResult(1)
-    }
-
-    @Test
-    fun getTocSuccessTest() {
-        val dummyFile = File("file://dummy/file")
-        val epubStream = EpubStream(dummyFile)
-        epubStream.getToc()
-                .test()
-                .assertResult(1)
-    }
-
-    @Test
-    fun getFileSuccessTest() {
-        val dummyFile = File("file://dummy/file")
-        val epubStream = EpubStream(dummyFile)
-        epubStream.getFile()
-                .test()
-                .assertResult(1)
-    }
-
 }
